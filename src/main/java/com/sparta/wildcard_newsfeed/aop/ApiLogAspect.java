7b7ecc90ce1330d538c105d6fdb20e985f805b93
Chanwon-Seo <scwonn60@gmail.com>
2024-06-13 21:46:22 +0900

    fix : AOP 불필요 코드 제거

--- src/main/java/com/sparta/wildcard_newsfeed/aop/ApiLogAspect.java
+++ src/main/java/com/sparta/wildcard_newsfeed/aop/ApiLogAspect.java
@@ -3,42 +3,17 @@ package com.sparta.wildcard_newsfeed.aop;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import java.lang.reflect.Method;
 
 @Slf4j(topic = "UseTimeAop")
 @Aspect
 @Component
 @RequiredArgsConstructor
 public class ApiLogAspect {
-
-    @Pointcut("execution(* com.sparta.wildcard_newsfeed.domain.comment.controller.*(..))")
-    private void comment() {
-    }
-
-    @Pointcut("execution(* com.sparta.wildcard_newsfeed.domain.liked.controller.*(..))")
-    private void liked() {
-    }
-
-    @Pointcut("execution(* com.sparta.wildcard_newsfeed.domain.post.controller.*(..))")
-    private void post() {
-    }
-
-    @Pointcut("execution(* com.sparta.wildcard_newsfeed.domain.token.controller.*(..))")
-    private void token() {
-    }
-
-    @Pointcut("execution(* com.sparta.wildcard_newsfeed.domain.user.controller.*(..))")
-    private void user() {
-    }
 
     @Before("within(@org.springframework.web.bind.annotation.RestController *)")
     public void logBefore() {

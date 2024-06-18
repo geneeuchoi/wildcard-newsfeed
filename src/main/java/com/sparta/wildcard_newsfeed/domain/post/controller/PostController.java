1b36efbcb88bc336a246b705ff03a188149f6fa4
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-06-18 11:45:51 +0900

    REFACTOR: 안쓰는 객체 삭제

--- src/main/java/com/sparta/wildcard_newsfeed/domain/post/controller/PostController.java
+++ src/main/java/com/sparta/wildcard_newsfeed/domain/post/controller/PostController.java
@@ -20,6 +20,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -37,7 +38,6 @@ public class PostController {
 
     private final PostService postService;
     private final CommentService commentService;
-    private final PostRepository postRepository;
 
     // 게시물 등록
     @PostMapping

b83099d29efac420f8884bd7e72cd7f8ece51725
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-06-18 10:33:58 +0900

    REFACTOR: @repository 추가

--- src/main/java/com/sparta/wildcard_newsfeed/domain/post/repository/PostMediaRepository.java
+++ src/main/java/com/sparta/wildcard_newsfeed/domain/post/repository/PostMediaRepository.java
@@ -2,9 +2,11 @@ package com.sparta.wildcard_newsfeed.domain.post.repository;
 
 import com.sparta.wildcard_newsfeed.domain.post.entity.PostMedia;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PostMediaRepository extends JpaRepository<PostMedia, Long> {
     List<PostMedia> findByPostId(Long postId);
 }
--- src/main/java/com/sparta/wildcard_newsfeed/domain/post/repository/PostRepository.java
+++ src/main/java/com/sparta/wildcard_newsfeed/domain/post/repository/PostRepository.java
@@ -7,7 +7,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query(value = "select p.id as postId, u.id as userId, p.title, p.content, u.name, " +
